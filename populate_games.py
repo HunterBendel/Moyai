425a9cb42f4bb288c735bdc6b8388a9eecaed97a
Hunter Bendel <70610577+HunterBendel@users.noreply.github.com>
2024-03-18 02:36:01 +0000

    Script to populate the game table with thousands of video games.

---
+++ populate_games.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+
+def fetch_steam_apps():
+    url = 'https://api.steampowered.com/ISteamApps/GetAppList/v2/'
+    response = requests.get(url)
+    data = response.json()
+    return data['applist']['apps']
+
+def fetch_app_details(app_id):
+    url = f'https://store.steampowered.com/api/appdetails?appids={app_id}'
+    response = requests.get(url)
+    data = response.json()
+    if data and str(app_id) in data and 'data' in data[str(app_id)] and data[str(app_id)]['success']:
+        return data[str(app_id)]['data']
+    return None
+
+def fetch_cover_image_url(app_id):
+    store_url = f"https://store.steampowered.com/app/{app_id}"
+    response = requests.get(store_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    cover_image_tag = soup.find('img', class_='game_header_image_full')
+    if cover_image_tag:
+        return cover_image_tag['src']
+    return None
+
+def insert_game_into_db(game_name, cover_image_url):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO game (name, cover_image_url) VALUES (?, ?)", (game_name, cover_image_url))
+    conn.commit()
+    conn.close()
+
+def populate_game_table():
+    apps = fetch_steam_apps()
+    for app in apps:
+        details = fetch_app_details(app['appid'])
+        if details and details['type'] == 'game':
+            cover_image_url = fetch_cover_image_url(app['appid'])
+            if cover_image_url:
+                insert_game_into_db(details['name'], cover_image_url)
+
+if __name__ == '__main__':
+    populate_game_table()
